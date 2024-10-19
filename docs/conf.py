aeeb96fd8078aca09255fd33bdf6329dcbe94577
Arie Bovenberg <a.c.bovenberg@gmail.com>
2024-10-19 15:44:55 +0200

    docs improvements

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-
 # -- Project information -----------------------------------------------------
 import importlib.metadata
 
@@ -20,13 +11,6 @@ release = metadata["Version"]
 
 # -- General configuration ---------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -34,68 +18,24 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
 source_suffix = ".rst"
-
-# The master toctree document.
 master_doc = "index"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "tango"
-
 autodoc_member_order = "bysource"
-
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_material"
-
-# html_static_path = ['_static']
+autodoc_member_order = "bysource"
+html_theme = "furo"
 highlight_language = "python3"
-
-# Material theme options (see theme.conf for more information)
-html_theme_options = {
-    "nav_title": "🦆 Quacks",
-    # Set the color and the accent color
-    "color_primary": "teal",
-    "color_accent": "green",
-    # Set the repo location to get a badge with stats
-    "repo_url": "https://github.com/ariebovenberg/quacks/",
-    "repo_name": "Quacks",
-    # Visible levels of the global TOC; -1 means unlimited
-    "globaltoc_depth": 3,
-    # If False, expand all TOC entries
-    "globaltoc_collapse": False,
-    # If True, show hidden TOC entries
-    "globaltoc_includehidden": False,
-}
-
-
-html_sidebars = {
-    "**": [
-        "logo-text.html",
-        "globaltoc.html",
-        "localtoc.html",
-        "searchbox.html",
-    ]
-}
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
+pygments_style = "default"
+pygments_dark_style = "lightbulb"
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
+toc_object_entries_show_parents = "hide"
